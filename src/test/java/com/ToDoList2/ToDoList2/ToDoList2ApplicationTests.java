df2fbd19b9f57e22c54f04a00e951a3d90f2daa0
Maykr1 <eclark72713@gmail.com>
2025-06-21 11:57:21 -0400

    Fixed contextLoads test

--- src/test/java/com/ToDoList2/ToDoList2/ToDoList2ApplicationTests.java
+++ src/test/java/com/ToDoList2/ToDoList2/ToDoList2ApplicationTests.java
@@ -2,11 +2,29 @@ package com.ToDoList2.ToDoList2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@Import(TestcontainersConfiguration.class)
 @SpringBootTest
+@Testcontainers
 class ToDoList2ApplicationTests {
+	@SuppressWarnings("resource")
+	@Container
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:17.5")
+		.withDatabaseName("testdb")
+		.withUsername("postgres")
+		.withPassword("1234");
+
+	@DynamicPropertySource
+	static void overrideProps(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
+	
 
 	@Test
 	void contextLoads() {
