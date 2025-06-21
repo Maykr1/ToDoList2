a9d7931506aee3ce1edf5f7cb13ac0880ab47455
Maykr1 <eclark72713@gmail.com>
2025-06-21 11:11:05 -0400

    Changed the version in the Controller

--- src/main/java/com/ToDoList2/ToDoList2/controller/ToDoController.java
+++ src/main/java/com/ToDoList2/ToDoList2/controller/ToDoController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 
 @Slf4j
 @RestController
-@RequestMapping("/ToDo")
+@RequestMapping("/ToDo/v2")
 @RequiredArgsConstructor
 public class ToDoController {
     private final ToDoService toDoService;
