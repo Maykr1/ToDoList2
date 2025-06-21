33fb19981eba4ed81f6b28fcdf5686dcd948b007
Maykr1 <eclark72713@gmail.com>
2025-06-21 12:16:01 -0400

    Removed unused imports in controller

--- src/main/java/com/ToDoList2/ToDoList2/controller/ToDoController.java
+++ src/main/java/com/ToDoList2/ToDoList2/controller/ToDoController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ToDoList2.ToDoList2.entity.ToDo;
-import com.ToDoList2.ToDoList2.exception.ResourceNotFoundException;
 import com.ToDoList2.ToDoList2.service.ToDoService;
 
 import lombok.RequiredArgsConstructor;
@@ -13,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
