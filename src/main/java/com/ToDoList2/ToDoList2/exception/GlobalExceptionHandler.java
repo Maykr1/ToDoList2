bb16d0b94dd72722d7ea9fdf87e41736d6673757
Maykr1 <eclark72713@gmail.com>
2025-06-23 19:06:53 -0400

    GlobalExceptionHandler was preferred by the Sprnig Boot App, trying to prevent that from happening with adding which classes GlobalExceptionHandler can only respond to

--- src/main/java/com/ToDoList2/ToDoList2/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ToDoList2/ToDoList2/exception/GlobalExceptionHandler.java
@@ -3,12 +3,13 @@ package com.ToDoList2.ToDoList2.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestControllerAdvice
+@RestControllerAdvice(annotations = RestController.class)
 public class GlobalExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException e) {
