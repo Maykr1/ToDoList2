a171de15b051ace3db330591f1478dd0ce2dda50
Maykr1 <eclark72713@gmail.com>
2025-06-23 19:15:31 -0400

    Time for debugging

--- src/main/java/com/ToDoList2/ToDoList2/exception/HtmlExceptionHandler.java
+++ src/main/java/com/ToDoList2/ToDoList2/exception/HtmlExceptionHandler.java
@@ -6,16 +6,21 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.ToDoList2.ToDoList2.controller.PageController;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @ControllerAdvice(assignableTypes = PageController.class)
 public class HtmlExceptionHandler {
     @ExceptionHandler(ResourceNotFoundException.class)
     public String handleResourceNotFoundException(ResourceNotFoundException e, RedirectAttributes redirectAttributes) {
+        log.error("[ RESOURCE_NOT_FOUND_EXCEPTION ] - An error occured: {}", e.getMessage());
         redirectAttributes.addFlashAttribute("popupErrorMessage", e.getMessage());
         return "redirect:/ToDo/v2";
     }
 
     @ExceptionHandler(Exception.class)
     public String handleAll(Exception e, RedirectAttributes redirectAttributes) {
+        log.error("[ EXCEPTION ] - An error occured: {}", e.getMessage());
         redirectAttributes.addFlashAttribute("popupErrorMessage", e.getMessage());
         return "redirect:/ToDo/v2";
     }
