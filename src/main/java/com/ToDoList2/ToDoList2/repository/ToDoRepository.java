74be528599e2679a1f5f452df08eb662827c95d7
Ethan Clark <eclark72713@gmail.com>
2025-07-12 16:32:27 -0400

    Created new repository method

--- src/main/java/com/ToDoList2/ToDoList2/repository/ToDoRepository.java
+++ src/main/java/com/ToDoList2/ToDoList2/repository/ToDoRepository.java
@@ -1,9 +1,11 @@
 package com.ToDoList2.ToDoList2.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.ToDoList2.ToDoList2.entity.ToDo;
 
 public interface ToDoRepository extends JpaRepository<ToDo, Integer> {
-    
+    public List<ToDo> findAllByCustomUserUsername(String username);
 }
