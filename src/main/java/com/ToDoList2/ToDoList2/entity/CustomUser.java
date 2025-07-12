9f585fb16f81b1ad28cbc1b05c794834e314b608
Ethan Clark <eclark72713@gmail.com>
2025-07-12 16:22:22 -0400

    Modified Entities

--- src/main/java/com/ToDoList2/ToDoList2/entity/CustomUser.java
+++ src/main/java/com/ToDoList2/ToDoList2/entity/CustomUser.java
@@ -1,9 +1,15 @@
 package com.ToDoList2.ToDoList2.entity;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -23,4 +29,9 @@ public class CustomUser {
 
     @Column(name = "password")
     private String password;
+
+    @OneToMany
+    @JsonManagedReference
+    @JoinColumn(name = "customUser_id")
+    private List<ToDo> todoList;
 }
--- src/main/java/com/ToDoList2/ToDoList2/entity/ToDo.java
+++ src/main/java/com/ToDoList2/ToDoList2/entity/ToDo.java
@@ -4,10 +4,14 @@ import java.time.LocalDate;
 
 import org.springframework.format.annotation.DateTimeFormat;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -34,4 +38,9 @@ public class ToDo {
 
     @Column(name="COMPLETED")
     private Boolean completed;
+
+    @ManyToOne
+    @JoinColumn(name = "customUser_id")
+    @JsonBackReference
+    private CustomUser customUser;
 }
